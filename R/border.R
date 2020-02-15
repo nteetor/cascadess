277130383cefc72c3aef0dc576a1330d3cf28dcb
nteetor <nathanteetor@gmail.com>
2020-02-15 11:55:54 -0500

    chore(border): move sides() helper

--- R/border.R
+++ R/border.R
@@ -1,3 +1,25 @@
+sides <- function(all, top, right, bottom, left) {
+  s <- list(top, right, bottom, left)
+  true <- which_true(s)
+  null <- which_null(s)
+
+  if (all(null | !true)) {
+    return(NULL)
+  }
+
+  possible <- c("top", "right", "bottom", "left")
+
+  if (is_false(all)) {
+    possible[true]
+  } else if (is_true(all)) {
+    possible[null | true]
+  } else {
+    all <- grep(paste0("^", all, collapse = "|"), possible, value = TRUE)
+
+    unique(if (any(true)) c(all, possible[true]) else all)
+  }
+}
+
 #' Borders and border colors
 #'
 #' The `border()` functions adds or modifies tag element borders.
--- R/utils.R
+++ R/utils.R
@@ -8,28 +8,6 @@ dash <- function(...) {
   exec(paste, !!!args, sep = "-")
 }
 
-sides <- function(all, top, right, bottom, left) {
-  s <- list(top, right, bottom, left)
-  true <- which_true(s)
-  null <- which_null(s)
-
-  if (all(null | !true)) {
-    return(NULL)
-  }
-
-  possible <- c("top", "right", "bottom", "left")
-
-  if (is_false(all)) {
-    possible[true]
-  } else if (is_true(all)) {
-    possible[null | true]
-  } else {
-    all <- grep(paste0("^", all, collapse = "|"), possible, value = TRUE)
-
-    unique(if (any(true)) c(all, possible[true]) else all)
-  }
-}
-
 named_list <- function(...) {
   lapply(quos(..., .named = TRUE), eval_tidy, env = caller_env())
 }
