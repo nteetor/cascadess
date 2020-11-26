932a6d5b854b3be7815adb9c720fc9c74381c9bd
nteetor <nate@haufin.ch>
2020-11-25 21:27:15 -0600

    chore(scripts): use dev {pkgdown} for bin/site.sh

--- bin/site.sh
+++ bin/site.sh
@@ -3,6 +3,9 @@
 DIR=$(pwd)
 PKGDOWN="$DIR/pkgdown"
 
+Rscript -e "remotes::install_github('nteetor/pkgdown@master-nt-preserve-html')"
+Rscript -e "devtools::install(quick = TRUE, build = FALSE, upgrade = FALSE, keep_source = TRUE)"
+
 Rscript -e "\
 rmarkdown::render(
 knit_root_dir = '$PKGDOWN',
@@ -19,5 +22,8 @@ Rscript -e "pkgdown::build_site(
     destination = '$PKGDOWN/site'
   )
 )"
+
+# not sure why pkgdown is creating nested site folders
+rm -rf $PKGDOWN/site/site
 
 open $PKGDOWN/site/index.html
